ee03b41f8d73cb120cdf427409e19fd55d6520d0
leona5186 <leona5186workmail@gamil.com>
2025-06-21 15:52:20 +0800

    Update preprocessing to include 'weekday' feature

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,18 +1,31 @@
 import pandas as pd
 
-# read raw data
-df = pd.read_csv('data/coffeerawdataset1.csv')
+# data read
+df = pd.read_csv("data/coffeerawdataset1.csv")
 
-# show row number
-print(f"Original data shape: {df.shape}")
-
-# delete duplicate value
+# remove duplicate value
 df_cleaned = df.drop_duplicates()
 
-# represent row number
-print(f"Cleaned data shape: {df_cleaned.shape}")
+# convert transaction_time to datetime
+df_cleaned['transaction_time'] = pd.to_datetime(
+    df_cleaned['transaction_time'],
+    format='%I:%M:%S %p',
+    errors='coerce'
+)
+# extract weekday from transaction date
+df_cleaned["weekday"] = pd.to_datetime(df_cleaned["transaction_date"]).dt.weekday
 
-# save preprocessed data
-df_cleaned.to_csv('data/preprocessed_dataset.csv', index=False)
 
-print("Preprocessed dataset saved to data/preprocessed_dataset.csv")
+# add new column：keep hour + AM/PM
+df_cleaned['transaction_hour'] = df_cleaned['transaction_time'].dt.strftime('%I %p')
+
+# calculate sales amount
+df_cleaned['sales_amount'] = df_cleaned['transaction_qty'] * df_cleaned['unit_price']
+
+# present data shape
+print(f"✅ Cleaned data shape: {df_cleaned.shape}")
+
+# save processed data
+df_cleaned.to_csv("data/preprocessed_dataset.csv", index=False)
+print("✅ Preprocessed dataset saved to data/preprocessed_dataset.csv")
+
